e1e6ecc5c62ea15fbf6e47069d7f2c1d60f92d09
kartik jajoria <100478521+Kartikjajoria@users.noreply.github.com>
2023-09-29 23:23:55 +0530

    Update q4.cpp

--- q4.cpp
+++ q4.cpp
@@ -4,12 +4,12 @@ int main()
 {
 	cout<<"Name = kartik jajoria"<<endl<<"enrollment no = 08129802021"<<endl;
 	float frac,a;
-	int integ;
+	int integer;
 	cout<<"enter no in decimal form"<<endl;
 	cin >>a;
 	integ = int(a);
 	frac= a-integ;
-	cout<<"integer part is "<< integ<< " Fraction Part is "<<frac;
+	cout<<"integer part is "<< integer<< " Fraction Part is "<<frac;
 	
 }
 
