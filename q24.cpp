8cd4669d5c20183f3f50a855a9b635fbfc533726
kartik jajoria <100478521+Kartikjajoria@users.noreply.github.com>
2023-10-02 22:26:23 +0530

    Update q24.cpp

--- q24.cpp
+++ q24.cpp
@@ -10,7 +10,7 @@ class bank
         char nm[100], acctype[100];
         float bal;  
    public:
-        bank(int acc_no, char *name, char *acc_type, float balance)  //Parameterized Constructor
+        bank(int acc_no, char *name, char *acc_type, float balance)  // this is a Parameterized Constructor
         {
                 acno=acc_no;
                 strcpy(nm, name);
